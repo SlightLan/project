bc8df26d4c21a4ef5317d3dacee04a5a569a4875
Lan <324138594@qq.com>
2018-03-05 16:58:49 +0800

    完善评论点赞功能

--- app/controllers/star_controller.rb
+++ app/controllers/star_controller.rb
@@ -1,12 +1,16 @@
 class StarController < ApplicationController
 	def star_question
-		@question=Question.find_by(:id => params[:id])
+		@question=Question.find_by(:id => params[:question])
 		@question.stars += 1
 		@question.save
 		redirect_to @question
 	end
 
 	def star_comment
-		redirect_to question_path(:id => params[:id])
+		@comment=Comment.find_by(:id => params[:comment])
+		@comment.stars += 1
+		@comment.save
+		@question=Question.find_by(:id => params[:id])
+		redirect_to @question
 	end
 end
