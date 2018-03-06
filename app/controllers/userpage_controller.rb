f49a52fe65d51bef95864bfb95ec11c76ec374b7
shiyan <824097378@qq.com>
2018-03-06 19:37:59 +0800

    user

--- app/controllers/userpage_controller.rb
+++ app/controllers/userpage_controller.rb
@@ -1,4 +1,15 @@
 class UserpageController < ApplicationController
 	def show
+		@user=current_user
+		@questions=Question.where(asker: @user.email)
+		@comments=Comment.where(commenter: @user.email)
+	end
+
+	def info
+		@user=current_user
+	end
+	def user_params
+		params.require(:user).permit(:name, :email, :password,
+																 :password_confirmation)
 	end
 end
